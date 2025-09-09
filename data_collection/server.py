fd131be736f5450616aa1133c91cac8b4336e0d4
Jeongmin <78248064+jeong1010@users.noreply.github.com>
2025-09-10 01:42:41 +0900

    수신파트 수정중

--- data_collection/server.py
+++ data_collection/server.py
@@ -1,5 +1,7 @@
 # 서버
 from socket import *
+import numpy as np
+import os
 
 serverPort = 1234
 serverSocket = socket(AF_INET, SOCK_DGRAM)
@@ -10,25 +12,40 @@ print('ready to receive')
 
 cnt = 0
 data = []
+MAX_LEN = 80
+FEATURES_LEN = 6
+
 while True:
     message, clientAddress = serverSocket.recvfrom(1024) # 받을 때까지 블록킹.
     cnt += 1
-    modifiedData = message.decode()#.split(' ')[1]
+    receivedData = message.decode()
 
     # 마지막 패킷
-    if ',' not in modifiedData:
-        if cnt == int(modifiedData) and int(modifiedData) <= 80:
-            ### csv로 저장하는 코드.
-            print('# 저장하였습니다')
+    if ',' not in receivedData:
+        total_packets = int(receivedData)
+
+        if cnt != total_packets:
+            print('# 저장실패 - 패킷 loss')
+        elif total_packets <= MAX_LEN:
+            print('# 저장실패 - 최대 입력 초과')
         else:
-            print('# 저장실패')
-        # 리스트 비우기
+            ### csv로 저장하는 코드 *
+            seq = np.array(data, dtype=np.float32)
+
+            padded = np.zeros((MAX_LEN, FEATURES_LEN), dtype=np.float32)
+            
+
+            print('# 저장성공')
+            
+        # 리스트 비우고, 다시 루프
         data = []
+        cnt = 0
+        continue
     # 리스트에 누적 
     else:
-       
-    # print(clientAddress, ': recv data = ' + str(modifiedData))
+        datas = receivedData.split(',')
+        data.append(datas)
+
     #### 받은 데이터 리스트로 누적하고, 최종 리스트를 .csv에 append ####
-    # 그리고, 마지막에 받는 토탈 데이터 (len == 1인 데이터)랑 최종 리스트 길이랑 다르면 loss난 거로 간주하고, 저장안함.
 
 
